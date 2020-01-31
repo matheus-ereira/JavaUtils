3cb4ac3645413c4af6cf16a172dd36805ec8fff6
Matheus <matheus.psilva@datafast.com.br>
2020-01-31 16:50:25 -0200

    Test for the new implementation.

--- src/test/java/com/util/test/StringUtil/StringUtilTestOnlyNumber.java
+++ src/test/java/com/util/test/StringUtil/StringUtilTestOnlyNumber.java
@@ -83,6 +83,7 @@ public class StringUtilTestOnlyNumber {
         assertNotNull(result1);
         assertNotNull(result2);
         assertNotNull(result3);
+        
         assertEquals("123544554114", result1);
         assertEquals("2151151251", result2);
         assertEquals("144141414141", result3);
@@ -104,8 +105,31 @@ public class StringUtilTestOnlyNumber {
         assertNotNull(result1);
         assertNotNull(result2);
         assertNotNull(result3);
+        
         assertEquals("", result1);
         assertEquals("", result2);
         assertEquals("", result3);
+    }
+    
+    @Test
+    public void StringUtilTest_OnlyNumber_String_With_White_Spaces_and_Numbers_Return_Clean() {
+        //Arrange
+        String text1 = "     1 2    3 4 5    7 8 9     12";
+        String text2 = "1245      78    96      456   147    561   12323";
+        String text3 = "51   515 1561      545    651  5 611 15     51 55 1 151  1 41 45  151 ";
+
+        //Act
+        String result1 = onlyNumber(text1);
+        String result2 = onlyNumber(text2);
+        String result3 = onlyNumber(text3);
+
+        //Result
+        assertNotNull(result1);
+        assertNotNull(result2);
+        assertNotNull(result3);
+        
+        assertEquals("1234578912", result1);
+        assertEquals("1245789645614756112323", result2);
+        assertEquals("5151515615456515611155155115114145151", result3);
     }
 }
